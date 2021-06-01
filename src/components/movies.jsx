b0b99437178550b2e13467606ad1443a4de24191
Yohan Chathuranga <yohanchathuranga72565@gmail.com>
2021-06-01 17:29:16 +0530

    Added movie form with validation using Joi library

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -6,6 +6,7 @@ import ListGroup from './common/listGroup';
 import {paginate} from '../utils/paginate';
 import {getGenres} from '../services/fakeGenreService';
 import _ from 'lodash';
+import {Link} from 'react-router-dom';
 
 
 class Movies extends Component {
@@ -78,6 +79,13 @@ class Movies extends Component {
                 />
             </div>
             <div className="col">
+                <Link
+                    to = "/movies/new"
+                    className = "btn btn-primary"
+                    style = {{marginBottom: 20 }}
+                >
+                 New Movie
+                </Link>
                 <p>Showing {totalCount} movies in the database.</p>
                 <MoviesTable
                     movies = {movies}
